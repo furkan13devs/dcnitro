8eae7c3e47837d0cce9d3b4999d538330eb6deb3
burak <121892854+swantexq@users.noreply.github.com>
2023-05-15 22:14:17 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,10 +1,14 @@
-chars = "abcdefghijklmnoprstuvyzqwABCDEFGHIJKLMNOPRSTUVYQW1234567890"
+''')
+
+
+		chars = "abcdefghijklmnoprstuvyzqwABCDEFGHIJKLMNOPRSTUVYQW1234567890"
 
 		while 1:
-			password_sayi = int(input("NİTRO SAYISI GİR /CODED BY SWANTEX "))
+			password_sayi = int(input("Kaç tane oluşturulsun? "))
 			for x in range(0,password_sayi):
 				password = ""
 				for x in range(16):
 					password_sayi = random.choice(chars)
 					password	= password + password_sayi
 				print("discord.gift/"+password)
+				''')
